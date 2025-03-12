8ce2d0a7fab61272172b225346010d02f28417a1
Fernando Diaz <fernydiaz62@gmail.com>
2025-03-12 03:20:21 -0300

    Update customer DTO to handle potential null values and modify environment token

--- src/features/customers/dto/create-customer.dto.ts
+++ src/features/customers/dto/create-customer.dto.ts
@@ -5,36 +5,36 @@ export class CreateCustomerDto {
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => value?.trim())
   name: string;
 
   @IsString()
   @MaxLength(255)
   @IsOptional()
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => value?.trim())
   dni?: string;
 
   @IsString()
   @MaxLength(255)
   @IsOptional()
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => value?.trim())
   address?: string;
 
   @IsString()
   @MaxLength(255)
   @IsOptional()
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => value?.trim())
   city?: string;
 
   @IsString()
   @MaxLength(255)
   @IsOptional()
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => value?.trim())
   email?: string;
 
   @IsString()
   @MaxLength(255)
   @IsOptional()
-  @Transform(({ value }) => value.trim())
+  @Transform(({ value }) => value?.trim())
   phone?: string;
 }
